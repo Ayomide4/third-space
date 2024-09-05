2d3303d8dc047cb6cada5e2a8524e0e759a89117
Ayo Omotosho <aomotosho4@gmail.com>
2024-09-05 06:25:37 -0500

    add recommended and howto

--- app/components/recommended.tsx
+++ app/components/recommended.tsx
@@ -2,20 +2,43 @@ import Image from "next/image";
 import library from "../../public/assets/library.jpg";
 import coffee from "../../public/assets/1418coffee.webp";
 import pub from "../../public/assets/pub.png";
+import white_star from "../../public/assets/white_star.svg";
+
 export default function Recommended() {
   return (
-    <div className="flex border-2 border-red-800 w-full justify-evenly gap-20">
-      <Image src={pub} className="flex-auto w-60 h-72 rounded" alt="pub" />
-      <Image
-        src={library}
-        className="flex-auto w-72 h-72 rounded"
-        alt="library"
-      />
-      <Image
-        src={coffee}
-        className="flex-auto w-72 h-72 rounded"
-        alt="coffee"
-      />
+    <div className="flex w-full justify-evenly ">
+      <div className="relative">
+        <div className="absolute bottom-2 left-2 bg-black rounded-lg p-2">
+          <p className="text-white">Fillmore Pub</p>
+        </div>
+        <div className="absolute top-2 right-2 bg-accent rounded-lg p-2 flex items-center">
+          <Image src={white_star} className="w-5 h-5 mb-1 mr-1" alt="star" />
+          <p className="text-white">4.5</p>
+        </div>
+        <Image src={pub} className="w-60 h-60 rounded-lg" alt="pub" />
+      </div>
+
+      <div className="relative">
+        <div className="absolute bottom-2 left-2 bg-black rounded-lg p-2">
+          <p className="text-white">Library</p>
+        </div>
+        <div className="absolute top-2 right-2 bg-accent rounded-lg p-2 flex items-center">
+          <Image src={white_star} className="w-5 h-5 mb-1 mr-1" alt="star" />
+          <p className="text-white">4.5</p>
+        </div>
+        <Image src={library} className="w-60 h-60 rounded-lg" alt="library" />
+      </div>
+
+      <div className="relative">
+        <div className="absolute bottom-2 left-2 bg-black rounded-lg p-2">
+          <p className="text-white">1418 Coffee</p>
+        </div>
+        <div className="absolute top-2 right-2 bg-accent rounded-lg p-2 flex items-center">
+          <Image src={white_star} className="w-5 h-5 mb-1 mr-1" alt="star" />
+          <p className="text-white">4.5</p>
+        </div>
+        <Image src={coffee} className="w-60 h-60 rounded-lg" alt="coffee" />
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import location from "../public/assets/location.svg";
 import glass from "../public/assets/glass.svg";
-import cancel from "../public/assets/cancel.svg";
 import Profile from "./components/profile";
 import Link from "next/link";
 import Recommended from "./components/recommended";
+import arrow_down from "../public/assets/arrow_down.svg";
 
 //TODO:
 //- change color of location icon to primary
@@ -13,8 +13,8 @@ import Recommended from "./components/recommended";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-10 md:p-20 bg-background relative ">
-      <div className="flex flex-col  w-full  md:justify-between md:flex-row-reverse md:mt-10 md:items-center">
+    <main className="flex min-h-screen flex-col items-center md:justify-between p-10 md:px-24 bg-background relative ">
+      <div className="flex flex-col  w-full md:h-full md:justify-between md:flex-row-reverse md:mt-10 md:items-center">
         <Link href="/auth" className="w-fit self-end mb-20 md:mb-0">
           <Profile background="grey" />
         </Link>
@@ -22,8 +22,8 @@ export default function Home() {
           Vibe Finder
         </h1>
       </div>
-      <div className="flex flex-col items-center mb-64 w-full border border-black ">
-        <div className="flex border-black border bg-grey rounded-md p-4 w-full h-16 items-center md:w-9/12">
+      <div className="flex flex-col items-center w-full mt-10 md:mt-0 md:flex-grow ">
+        <div className="flex border-black border bg-grey rounded-md p-4  md:mt-32 w-full h-16 items-center md:w-9/12">
           <Image
             className="w-6 h-6 cursor-pointer"
             src={glass}
@@ -39,9 +39,14 @@ export default function Home() {
             alt="location"
           />
         </div>
-        <div className="hidden w-full md:flex justify-center">
+        <div className="hidden w-4/6 md:flex flex-col mt-14 items-center">
+          <h2 className="font-semibold mb-4 self-start">Recommended</h2>
           <Recommended />
         </div>
+      </div>
+      <div className="flex flex-col items-center">
+        <h2 className="font-semibold">How this works</h2>
+        <Image src={arrow_down} className="w-6 h-6" alt="arrow down" />
       </div>
     </main>
   );
